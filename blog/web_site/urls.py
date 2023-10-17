e53c3e65dab6c2a5bfee63499a4e82793c8d045f
Kamol <sadullaevkamol2004@gmail.com>
2023-10-17 17:10:42 +0500

    added pagination and article_ditail

--- blog/web_site/urls.py
+++ blog/web_site/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name="home")
+    path('', views.home_view, name="home"),
+    path('categories/<str:category_id>/', views.category_articles, name='category_articles'),
+    path('articles/<str:article_id>/', views.article_ditail , name='article_ditail')
 ]
--- blog/web_site/views.py
+++ blog/web_site/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render, HttpResponse
+from .models import Article, Category
 
+from django.core.paginator import Paginator
 # Create your views here.
 
 def home_view(request):
-    return render(request, 'web_site/index.html')
+    articles = Article.objects.all()
+    paged = Paginator(articles, 3)
+    page = request.GET.get('page')
+    articles = paged.get_page(page)
+    context = {
+        'articles' : articles
+    }
+    return render(request, 'web_site/index.html', context)
+
+
+def category_articles(request, category_id):
+    category = Category.objects.get(pk=category_id)
+    articles = category.articles.all()
+    paged = Paginator(articles, 3)
+    page = request.GET.get('page')
+    articles = paged.get_page(page)
+    context = {
+        'articles': articles
+    }
+    return render(request, 'web_site/index.html', context)
+
+
+def article_ditail(request, article_id):
+    article = Article.objects.get(pk=article_id)
+    context = {
+        "article": article
+    }
+    return render(request, "web_site/article_ditail.html", context)
+
+
 
 
